dc2d197946c24dd770218e90a629d45a40f14f6b
iSenity1812 <pvduchuya4@gmail.com>
2026-03-21 20:38:38 +0700

    fix update foods feature to route to food detail

--- routes/view.js
+++ routes/view.js
@@ -233,7 +233,7 @@ router.post('/page/foods', authenticateView, async (req, res) => {
 });
 
 // Update Food Route
-router.post('/page/foods/:id/update', authenticateView, async (req, res) => {
+router.post('/page/foods/:id', authenticateView, async (req, res) => {
     try {
         const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
         const foodId = req.params.id;
